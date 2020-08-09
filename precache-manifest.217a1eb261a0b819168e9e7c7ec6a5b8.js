9eb0b7f1154b23461faa19df1e55274bfb9ef352
rohit kumar <rohit1071998@gmail.com>
2020-08-10 00:22:11 +0530

    Updates

---
+++ precache-manifest.217a1eb261a0b819168e9e7c7ec6a5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda94fa39ca5cd83f05e4cf73521bc24",
+    "url": "/favorite-movies/index.html"
+  },
+  {
+    "revision": "ef7794ec85ee2101d5cc",
+    "url": "/favorite-movies/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "d9b47e4f947a67226901",
+    "url": "/favorite-movies/static/js/2.7692c723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/favorite-movies/static/js/2.7692c723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7794ec85ee2101d5cc",
+    "url": "/favorite-movies/static/js/main.6576a04e.chunk.js"
+  },
+  {
+    "revision": "636a8d4f71bd26d9673a",
+    "url": "/favorite-movies/static/js/runtime-main.72e60bd5.js"
+  }
+]);
