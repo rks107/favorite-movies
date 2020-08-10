521953658f88614307251b863d8bc049c5898b91
rohit kumar <rohit1071998@gmail.com>
2020-08-10 17:23:26 +0530

    Converting Curray Function Middleware to Curray arrow Fuction Middleware

--- src/index.js
+++ src/index.js
@@ -9,16 +9,22 @@ import rootReducer from './reducers';
 // Curry Function Logger(obj, next,action)
 // logger(obj)(next)(action)
 
-const logger = function({dispatch, getState}){
-  return function(next){
-    return function(action)
-    {
-      // Middleware code
-      console.log('ACTION_TYPE:', action.type);
-      next(action);
-    }
-  }
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  // Middleware code
+  console.log('ACTION_TYPE:', action.type);
+  next(action);
 }
+
+// const logger = function({dispatch, getState}){
+//   return function(next){
+//     return function(action)
+//     {
+//       // Middleware code
+//       console.log('ACTION_TYPE:', action.type);
+//       next(action);
+//     }
+//   }
+// }
 
 
 const store = createStore(rootReducer, applyMiddleware(logger));
